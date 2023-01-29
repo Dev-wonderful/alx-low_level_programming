8724a5d2715e5c1128cc5e2dcd35e03c9bd11937
Wonderful <adeonederful20@gmail.com>
2023-01-29 08:35:54 +0100

    optional

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * first - executes before the main function
+ *
+ * Return: nothing
+ */
+void first(void) __attribute__ ((constructor));
+
+void first(void)
+{
+	printf("You're beat! and yet, you must allow \n");
+	printf("I bore my house upon my back!\n");
+}
