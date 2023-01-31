648d543fdfeb555a054ebe65338a076afa08eb2d
Dev-wonderful <adeonederful20@gmail.com>
2023-01-31 13:20:45 -0800

    free a listint list, point head to NULL

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * free_listint2 - frees a listint_t list
+ * @head: a double pointer to a listint_t first node
+ *
+ * Return: nothing
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *temp;
+	listint_t *node;
+
+	if (*head == NULL)
+	{
+		return;
+	}
+
+	node = *head;
+
+	while (node->next != NULL)
+	{
+		temp = node->next;
+		free(node);
+		node = temp;
+	}
+
+	free(node);
+	*head = NULL;
+}
