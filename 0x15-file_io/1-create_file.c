ff91fa6555184715bee53f6b0353cd250ea2a59b
Dev-wonderful <adeonederful20@gmail.com>
2023-02-13 18:18:40 -0800

    create text file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,7 +1,7 @@
 #include <unistd.h>
 #include <fcntl.h>
 #include <stdlib.h>
-#include <strings.h>
+#include <string.h>
 #include "main.h"
 
 /**
@@ -15,11 +15,12 @@ int create_file(const char *filename, char *text_content)
 {
 	char *buf;
 	size_t len;
+	ssize_t wr_out;
 	int fd;
 
 	if (filename == NULL)
 		return (-1);
-	fd = open(*filename, O_WRONLY|O_CREAT|O_TRUNC, 600);
+	fd = open(filename, O_WRONLY|O_CREAT|O_TRUNC, 600);
 	if (fd < 0)
 		return (-1);
 	len = strlen(text_content);
@@ -28,5 +29,8 @@ int create_file(const char *filename, char *text_content)
 		return (-1);
 	wr_out = write(fd, buf, len);
 	free(buf);
-	return (wr_out);
+	if (wr_out < 0)
+		return(wr_out);
+
+	return (1);
 }
