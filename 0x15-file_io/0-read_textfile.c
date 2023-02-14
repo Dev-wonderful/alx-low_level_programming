7ea8d516c19208d0df3654da287e44d96a9aca2e
Dev-wonderful <adeonederful20@gmail.com>
2023-02-13 16:53:40 -0800

    read text file

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,37 @@
+#include <unistd.h>
+#include <fcntl.h>
+#include "main.h"
+
+/**
+ * read_textfile - read and prints nbytes of text file
+ * @filename: pointer to file path
+ * @letters: number of bytes to be read and printed
+ *
+ * Return: number of bytes read
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int fd;
+	ssize_t rd_output, wr_output;
+	char *final;
+
+	if (filename == NULL)
+		return (0);
+
+	fd = open(filename, O_RDWR);
+	if (fd < 0)
+		return (0);
+	final = malloc(sizeof(char) * letters);
+	if (final == NULL)
+		return (0);
+	rd_output = read(fd, final, letters);
+	if (rd_output < 0)
+		return (0);
+	wr_output = write(1, final, rd_output);
+	if (wr_output < 0)
+		return (0);
+	close(fd);
+	free(final);
+
+	return (rd_output);
+}
---
+++ 0x15-file_io/main.h
@@ -0,0 +1,7 @@
+#include <stdlib.h>
+#ifndef _MAIN_H_
+#define _MAIN_H_
+
+ssize_t read_textfile(const char *filename, size_t letters);
+
+#endif /*_MAIN_H_*/
