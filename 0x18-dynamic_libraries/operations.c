02071172a9513cd1f2c29eb72d04cf1880ad1fd4
Dev-wonderful <adeonederful20@gmail.com>
2023-03-20 10:19:02 -0700

    c library in python

---
+++ 0x18-dynamic_libraries/operations.c
@@ -0,0 +1,59 @@
+/**
+ * add - adds two numbers
+ * @a: first number
+ * @b: second number
+ *
+ * return: integer
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * sub - subtracts two numbers
+ * @a: first number
+ * @b: second number
+ *
+ * return: integer
+ */
+int sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * mul - multiplies two numbers
+ * @a: first number
+ * @b: second number
+ *
+ * return: integer
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * div - divides two numbers
+ * @a: first number
+ * @b: second number
+ *
+ * return: integer
+ */
+int div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * mod - modulus two numbers
+ * @a: first number
+ * @b: second number
+ *
+ * return: integer
+ */
+int mod(int a, int b)
+{
+	return (a % b);
+}
