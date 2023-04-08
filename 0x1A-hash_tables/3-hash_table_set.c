07faa238d8d9d1949510868705fb16469656fab6
Dev-wonderful <adeonederful20@gmail.com>
2023-04-08 20:05:22 +0000

    add/update hash table

---
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -0,0 +1,39 @@
+#include <stdlib.h>
+#include <string.h>
+#include "hash_tables.h"
+
+/**
+ * hash_table_set - adds an element to the hash table
+ * @ht: the hash table to be updated
+ * @key: the key for the value
+ * @value: the value being mapped
+ *
+ * Return: 1 for success, 0 for erro
+ */
+int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+{
+	unsigned long int index;
+	hash_node_t *new;
+
+	if (ht == NULL || key == NULL || value == NULL)
+	{
+		return (0);
+	}
+	else if (key[0] == '\0')
+	{
+		return (0);
+	}
+	index = key_index((const unsigned char *)key, ht->size);
+	new = malloc(sizeof(hash_node_t));
+	if (new == NULL)
+		return (0);
+	new->key = strdup(key);
+	if (new->key == NULL)
+		return (0);
+	new->value = strdup(value);
+	if (new->value == NULL)
+		return (0);
+	new->next = ht->array[index];
+	ht->array[index] = new;
+	return (1);
+}
