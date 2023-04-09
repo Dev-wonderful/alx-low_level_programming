39c57e82c7e30860c50ef8c17539efaf3d7b8f6e
Dev-wonderful <adeonederful20@gmail.com>
2023-04-09 17:53:24 +0000

    get value from hash table through key

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -12,6 +12,9 @@ char *hash_table_get(const hash_table_t *ht, const char *key)
 	unsigned long int index;
 	hash_node_t *tmp;
 
+	if (ht == NULL || key == NULL)
+		return (NULL);
+
 	index = key_index((const unsigned char *)key, ht->size);
 	tmp = ht->array[index];
 	while (tmp != NULL)
