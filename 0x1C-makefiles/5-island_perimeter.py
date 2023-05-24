5ecb5501cad5da42b7304cec8dd763b4901f875b
Wonderful <adeonederful20@gmail.com>
2023-05-24 22:36:48 +0100

    technical interview

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python3
 """a module to evaluate an island"""
 
+
 def island_perimeter(grid):
     """calculate the perimeter of an island"""
     length = len(grid) - 2
     width = len(grid[0]) - 2
     perimeter = length * width
     if perimeter <= 0:
-        return 0
+        return
     else:
         return perimeter
