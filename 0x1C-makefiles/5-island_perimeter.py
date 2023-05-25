6d31e8f7f5b11698ba819dc094be662b02a79625
Wonderful <adeonederful20@gmail.com>
2023-05-25 03:19:10 +0100

    interview

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -4,6 +4,22 @@
 
 def island_perimeter(grid):
     """calculate the perimeter of an island"""
+    lake = True
+    while lake:
+        island = grid[1:-1]
+        length = len(island)
+        border1 = grid[1]
+        border2 = grid[-2]
+        border3 = [island[i][1] for i in range(length)]
+        border4 = [island[i][-2] for i in range(length)]
+        border = border1[1:-1] + border2[1:-1] + border3 + border4
+        for cell in border:
+            if cell == 0:
+                print("better error")
+                lake = False
+                break
+        if lake:
+            return 0
     height = len(grid) - 2
     width = len(grid[0]) - 2
     if height > 100 or width > 100:
