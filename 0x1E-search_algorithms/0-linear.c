36b18478a154450e41518faa9040223210b44d87
Dev-wonderful <adeonederful20@gmail.com>
2023-07-12 00:40:05 +0000

    string correction

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -19,12 +19,12 @@ int linear_search(int *array, size_t size, int value)
 	{
 		if (array[i] == value)
 		{
-			printf("value checked array[%lu] = [%d]\n", i, array[i]);
+			printf("Value checked array[%lu] = [%d]\n", i, array[i]);
 			return (i);
 		}
 		else
 		{
-			printf("value checked array[%lu] = [%d]\n", i, array[i]);
+			printf("Value checked array[%lu] = [%d]\n", i, array[i]);
 		}
 	}
 
